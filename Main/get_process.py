5c744d202f74c71119fe902830bcb70a36cdc633
Witalij Krajewski <115834531+witalijkrajewski@users.noreply.github.com>
2022-12-14 08:27:37 +0300

    fix commit

--- Main/get_process.py
+++ Main/get_process.py
@@ -1,6 +1,18 @@
 from .run_process import run_operation
 
 
+def validate_input(value):
+    if value.isdigit():
+        user_choice = int(value)
+        return True
+    return False
+
+
+def convert_input_str_in_int(value):
+    if validate_input(value):
+        return int(value)
+
+
 def start_app():
     while True:
         print('\nChoose type of operation to run, please:')
@@ -12,8 +24,9 @@ def start_app():
 
         user_choice = input('Enter Operation number or \'0\' to close application:')
 
-        if int(user_choice) in [1, 2, 3, 4, 5]:
-            run_operation(int(user_choice))
-
-        if int(user_choice) == 0:
-            break
+        if validate_input(user_choice):
+            choice = convert_input_str_in_int(user_choice)
+            if choice in [1, 2, 3, 4, 5]:
+                run_operation(choice)
+            if choice == 0:
+                break
